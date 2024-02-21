bc38e19bcb5160e8b275a199e51875264a336747
nishaanth <nishaanth20.nn@gmail.com>
2024-02-21 16:43:33 +0530

    changes

--- src/com/org/Apprunner/Executor.java
+++ src/com/org/Apprunner/Executor.java
@@ -7,32 +7,36 @@ import com.org.users.EmployeeValidation;
 public class Executor {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to java Assessment..");
+		System.out.println("Welcome to java Assessment");
 		Scanner sc=new Scanner(System.in);
 		String ch="y";
 		int a = 0;
 		do {
-			System.out.println("Enter 1.login or 2.Register..");
-			int c=Integer.parseInt(sc.nextLine());
+			System.out.println("1.Login");
+			System.out.println("2.Register");
+			//int c=Integer.parseInt(sc.nextLine());
+			int c=sc.nextInt();
 			String userName,password,fullName;
 			EmployeeValidation emp=new EmployeeValidation();
 			switch(c)
 			{
 		    case 1:
-				System.out.println("Enter your userName...");
-				userName=sc.nextLine();
-				System.out.println("Enter your password..");
-				password=sc.nextLine();
+				System.out.println("Enter your UserName:");
+				userName=sc.next();
+				System.out.println("Enter your Password");
+				password=sc.next();
 		        if(emp.validate(userName,password))
 		        {
-		        	System.out.println("Enter you want to 1.Begin test or 2.logout");
-		        	switch(Integer.parseInt(sc.nextLine()))
+		        	System.out.println("Enter the option");
+		        	System.out.println("1.Begin Test");
+		        	System.out.println("2.Logout");
+		        	switch(sc.nextInt())
 		        	{
 		        	case 1:
 		        		BeginTest test=new BeginTest();
-		        		System.out.println("Start...");
+		        		
 		        		test.showQuestion();
-		        		System.out.println("your percentage is"+test.percentage()+"%");
+		        		System.out.println("Your percentage is"+test.percentage()+"%");
 		        		
 		        		break;
 		        	case 2:
@@ -43,21 +47,21 @@ public class Executor {
 		        }
 		        else
 		        {
-		        	System.out.println("User doesn't exits..");
+		        	System.out.println("User doesn't exists");
 		        }
 				break;
 			case 2:
-			System.out.println("Enter your userName...");
+			System.out.println("Enter your UserName:");
 			userName=sc.next();
-			if(emp.checkIfuserExist(userName))
+			while(emp.checkIfuserExist(userName))
 			{
-				System.out.println("The username alreay exits...\nplease give another name..");
+				System.out.println("The username alreay exists \nplease give another name..");
 				userName=sc.next();			
 			}
-			System.out.println("Enter your password..");
+			System.out.println("Enter your Password");
 			password=sc.next();	
-			System.out.println("Enter your Fullname...");
-			fullName=sc.nextLine();
+			System.out.println("Enter your Fullname:");
+			fullName=sc.next();
 			
 			emp.addEmployee(userName, password, fullName);
 	
@@ -69,8 +73,10 @@ public class Executor {
 	
 			}
 			System.out.println("do you want to continue(y/n)");
-			a = sc.nextInt();
-		}while(a == 1);
+//			/*
+//			 * String temp=sc.nextLine(); a = sc.nextInt();
+//			 */
+		}while(sc.next().charAt(0)=='y');
 	}
 }
 
--- src/com/org/test/BeginTest.java
+++ src/com/org/test/BeginTest.java
@@ -14,7 +14,17 @@ public class BeginTest {
 	static {
 		questionList = new ArrayList<Questions>();
 		
-		questionList.add(new Questions(1, "?????????????", "1)Java", "2)C", "3)C++", "4)Python", 1));
+		questionList.add(new Questions(1, "what did we learn today", "1)Java", "2)C", "3)C++", "4)Python", 1));
+		questionList.add(new Questions(2, "short form of Structured Query Language", "1)SWL", "2)UWL", "3)SQL", "4)MYSQL", 3));
+		questionList.add(new Questions(3, "Which of the following is not a Java features?", "1)Dynamic", "2)Architecture Neutral", "3)pointer", "4)oops", 3));
+		questionList.add(new Questions(4, "Number of primitive data types in Java are?", "1)6", "2)7", "3)8", "4)9", 3));
+		questionList.add(new Questions(5, "What is the size of float and double in java?", "1)32 and 64", "2)32 and 32", "3)64 and 64", "4)64 and 32", 1));
+		questionList.add(new Questions(6, "Which is a reserved word in the Java programming language?", "1)native", "2)subclasses", "3)reference", "4)array", 1));
+		questionList.add(new Questions(7, "Which is a valid keyword in java?", "1)interface", "2)string", "3)Float", "4)unsigned", 1));
+		questionList.add(new Questions(8, "Which is the valid declarations within an interface definition?", "1)public double methoda();", "2)public final double methoda();", "3)static void methoda(double d1);", "1)protected void methoda(double d1)", 3));
+		questionList.add(new Questions(9, "Which is a valid declarations of a String", "1)String s1 = null;", "2)String s2 = 'null';", "3)String s3 = (String) 'abc';", "4)String s4 = (String) '\\ufeed';", 1));
+		questionList.add(new Questions(10, "What is the numerical range of a char?", "1)-128 to 127", "2)-(215) to (215) - 1", "3)0 to 32767", "4)0 to 65535", 4));
+		
 		
 		
 	}
@@ -64,18 +74,20 @@ public class BeginTest {
 		
 	}
 	
-	public int percentage() {
+	public float percentage() {
 		Answer a;
-		int score = 0;
+		float score = 0;
+		float per;
+		int x=questionList.size();
 		for(Questions q1:questionList ) {
 			a=findAnswer(q1.getqId());
 			if(a.getAnswer()==q1.getAnswer()) {
 				score=score+1;
 			}
 		}
+		per=(score/x);
 		
-		
-		return score/questionList.size()*100;
+		return per*100;
 		
 	}
 	
