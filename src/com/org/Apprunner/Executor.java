17f93e99a4c0154b46332027820853a9cdf6af76
kanagalakshmi09 <kanagasaru09@gmail.com>
2024-02-21 13:19:19 +0530

    login

--- src/com/org/Apprunner/Executor.java
+++ src/com/org/Apprunner/Executor.java
@@ -1,9 +1,33 @@
 package com.org.Apprunner;
-
+import java.util.*;
+import java.com.org.users;
 public class Executor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to java Assessment..");
+		System.out.println("Enter 1.login or 2.Register..");
+		Scanner sc=new Scanner(System.in);
+		String userName,password,fullName;
+		
+		switch(sc.nextInt())
+		{
+	    case 1:
+		System.out.println("Enter your userName...");
+		userName=sc.next();
+		System.out.println("Enter your password..");
+		password=sc.next();
+
+		break;
+		case 2:
+		System.out.println("Enter your userName...");
+		userName=sc.next();
+		System.out.println("Enter your password..");
+		password=sc.next();	
+		System.out.println("Enter your Fullname...");
+		fullName=sc.nextline();
+		break;
+
 
 	}
 
