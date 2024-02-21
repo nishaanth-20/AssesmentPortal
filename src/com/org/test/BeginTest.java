0f40d255f012fb6f0b2a53e8054e573b2ff1faab
nishaanth <nishaanth20.nn@gmail.com>
2024-02-21 14:46:55 +0530

    begin test

--- src/com/org/test/BeginTest.java
+++ src/com/org/test/BeginTest.java
@@ -1,6 +1,7 @@
 package com.org.test;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import com.org.mcq.Answer;
 import com.org.mcq.Questions;
@@ -18,6 +19,7 @@ public class BeginTest {
 		
 	}
 	
+	
 	public Questions findQuestion(int id){
         for(Questions e:questionList){
             if(e.getqId()==id) return e;
@@ -26,9 +28,21 @@ public class BeginTest {
     }
 	
 	
-	void showQuestion(int id) {
-		Questions q=findQuestion(id);
+	public Answer findAnswer(int id){
+        for(Answer e:answerList){
+            if(e.getId()==id) return e;
+        }
+        return null;
+    }
+	
+	
+	void showQuestion() {
 		
+		int choice;
+		
+		Scanner s=new Scanner(System.in) ;
+		
+		for(Questions q:questionList) {
 		System.out.println("Question" + q.getqId());
 		System.out.println( q.getQuestion());
 		System.out.println( q.getOption1());
@@ -36,12 +50,32 @@ public class BeginTest {
 		System.out.println( q.getOption3());
 		System.out.println( q.getOption4());
 		
+		System.out.println("enter your choice");
+		choice=s.nextInt();
+		storeAnswer(q.getqId(), choice);
 		
+		}
+		s.close();
 	}
 	
 	void storeAnswer(int qid,int choice) {
 		answerList.add(new Answer(qid, choice));
 		
+		
+	}
+	
+	int percentage() {
+		Answer a;
+		int score = 0;
+		for(Questions q1:questionList ) {
+			a=findAnswer(q1.getqId());
+			if(a.getAnswer()==q1.getAnswer()) {
+				score=score+1;
+			}
+		}
+		
+		
+		return score;
 		
 	}
 	
