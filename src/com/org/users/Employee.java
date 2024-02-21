4fcb7a64f55e424b065d031ffbdb3a6ea0bf12cd
nishaanth <nishaanth20.nn@gmail.com>
2024-02-21 13:17:27 +0530

    user and uservarication

--- src/com/org/users/Employee.java
+++ src/com/org/users/Employee.java
@@ -1,5 +1,42 @@
 package com.org.users;
 
 public class Employee {
-
+	private String userName;
+	private String password;
+	private int assignmentPercentage;
+	private String fullName;
+	public Employee(String userName, String password, int assignmentPercentage, String fullName) {
+		super();
+		this.userName = userName;
+		this.password = password;
+		this.assignmentPercentage = assignmentPercentage;
+		this.fullName = fullName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getAssignmentPercentage() {
+		return assignmentPercentage;
+	}
+	public void setAssignmentPercentage(int assignmentPercentage) {
+		this.assignmentPercentage = assignmentPercentage;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	
+																																									
 }
--- src/com/org/users/EmployeeValidation.java
+++ src/com/org/users/EmployeeValidation.java
@@ -1,5 +1,53 @@
 package com.org.users;
 
-public class EmployeeValidation {
+import java.util.ArrayList;
 
+
+
+
+
+public class EmployeeValidation {
+	
+	static ArrayList<Employee> employeeList;
+	static {
+		employeeList = new ArrayList<Employee>();
+		employeeList.add(new Employee("raj", "12345",0, "rajesh"));
+		
+	}
+	
+	public boolean checkIfuserExist(String name){
+		for(Employee e:employeeList){
+			if(name.equals(e.getUserName())) return true;
+		}
+		return false;
+}
+	
+	public Employee findAdmin(String name){
+        for(Employee e:employeeList){
+            if(e.getUserName().equals(name)) return e;
+        }
+        return null;
+    }
+	
+	public Employee validate(String username, String password) {
+		Employee e;
+		if(checkIfuserExist(username)){
+			e=findAdmin(username);
+			e.getPassword().equals(password);
+			return e;
+		}
+		return null;
+	}
+	
+	public void addEmployee(String username,String password,String fullName ) {
+		employeeList.add(new Employee(username, password, 0, fullName));
+	
+	}
+		
+		
+		
+		
+		
+	
+	
 }
